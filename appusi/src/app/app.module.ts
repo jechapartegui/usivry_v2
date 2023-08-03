ac9e5603b22859d53b665f491cd924c5ba904b4d
J-E <jechapartegui@gmail.com>
2023-08-03 17:11:43 +0200

    0308-1

--- appusi/src/app/app.module.ts
+++ appusi/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadFileComponent } from './load-file/load-file.component';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  , LoadFileComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
